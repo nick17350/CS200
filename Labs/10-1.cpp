c09d9aaf702e084873c3f750580aa82685f97695
nick17350 <nwheele5@stumail.jccc.edu>
2017-01-26 19:43:48 -0600

    Add files via upload

---
+++ Labs/10-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int countDown = 10;
+
+	while (countDown > 0 )
+	{
+
+		cout << countDown << " ";
+		countDown--;
+	}
+	cout << "done";
+	while (true);
+}
