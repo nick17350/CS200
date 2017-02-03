2b52a18037b9f2908020cd279460587c4b8025f7
nick17350 <nwheele5@stumail.jccc.edu>
2017-02-02 20:01:32 -0600

    Add files via upload

---
+++ Labs/Lab6Project1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+		for (int count = 0; count < 20; count++)
+		{
+			cout << count << " ";
+		}
+		while (true);
+	return 0;
+	
+}
---
+++ Labs/Lab6Project2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int count = 10; count <= 100; count = count +5)
+	{
+		cout << count << " ";
+	}
+	while (true);
+	return 0;
+}
